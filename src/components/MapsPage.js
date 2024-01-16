f659a654b431085b3d8c757438c982f64e99f717
bjmohsen <mohsenbouajila108@gmail.com>
2024-01-16 11:22:54 +0100

    first commit

---
+++ src/components/MapsPage.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Container, Row, Col, Card } from 'react-bootstrap';
+import { GoogleMap, LoadScript } from '@react-google-maps/api';
+
+const mapStyles = {
+  height: '400px',
+  width: '100%',
+};
+
+const center = {
+  lat: 36.854020,
+  lng: 10.29,
+};
+
+const MapsPage = () => {
+  return (
+    <Container className="mt-5">
+      <Row>
+        <Col>
+          <h1> Google Maps Landing Page!</h1>
+        </Col>
+      </Row>
+      <Row className="mt-3">
+        <Col>
+          <Card>
+            <Card.Body>
+              <LoadScript googleMapsApiKey='YOUR_API_KEY'>
+                <GoogleMap mapContainerStyle={mapStyles} center={center} zoom={10}>
+                  {/* the code will run correctly but the google maps card will display a warning because of some 
+                  security restrictions related to API KEY   */}
+                </GoogleMap>
+              </LoadScript>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default MapsPage;
